e55aef082394dbb8982f1d6e66968ce5fdc2bd69
carhaas <carhaas1@gmail.com>
2015-05-18 10:45:16 +0200

    check if tmp files get deleted

--- rebol.py
+++ rebol.py
@@ -379,7 +379,10 @@ def main():
                     sys.stderr.write("weights after: %s\n" % str(weights))
 
                     # delete old weights_tmp
+                    sys.stderr.write("weights tmp: %s\n" % weights_tmp)
+                    sys.stderr.write("file exists prior?: %s\n" % os.path.isfile(weights_tmp))
                     os.remove(weights_tmp)
+                    sys.stderr.write("file exists after?: %s\n" % os.path.isfile(weights_tmp))
 
                     # sys.stderr.write("open files rebol.py: %s\n" % get_open_fds())
                     sys.stderr.write("-------------\n\n")
